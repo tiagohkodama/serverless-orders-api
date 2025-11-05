46df00c98fe0f046c032af6fed307973cb30a8a3
tiagohkodama <tiago.kodama@gmail.com>
2025-11-04 23:45:09 -0300

    chore: add ESLint + Prettier config, lint scripts and update deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,66 @@
 module.exports = {
-  env: { node: true, jest: true, es2022: true },
+  root: true,
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 2022, sourceType: 'module' },
+  parserOptions: {
+    // Not using a `project` here to avoid type-aware parsing which can
+    // produce warnings when TypeScript versions differ between tools.
+    sourceType: 'module',
+    ecmaVersion: 2022
+  },
   plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-  rules: {}
-}
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  env: {
+    node: true,
+    es2022: true,
+    jest: true
+  },
+  rules: {
+    'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    // Allow `any` in places for gradual typing/migration
+    '@typescript-eslint/no-explicit-any': 'off',
+    // Optional chaining followed by non-null assertion is used in this codebase;
+    // disable the rule to avoid unsafe false positives for now.
+    '@typescript-eslint/no-non-null-asserted-optional-chain': 'off'
+  },
+  ignorePatterns: ['dist/', 'node_modules/', 'coverage/', '.serverless/']
+};
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    // Not using a `project` here to avoid type-aware parsing which can
+    // produce warnings when TypeScript versions differ between tools.
+    sourceType: 'module',
+    ecmaVersion: 2022
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  env: {
+    node: true,
+    es2022: true,
+    jest: true
+  },
+  rules: {
+    'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    // Allow `any` in places for gradual typing/migration
+    '@typescript-eslint/no-explicit-any': 'off',
+    // Optional chaining followed by non-null assertion is used in this codebase;
+    // disable the rule to avoid unsafe false positives for now.
+    '@typescript-eslint/no-non-null-asserted-optional-chain': 'off'
+  },
+  ignorePatterns: ['dist/', 'node_modules/', 'coverage/', '.serverless/']
+};
